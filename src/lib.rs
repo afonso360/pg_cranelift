f3383a04f26b001c9522b9cbc4f9d7ccfae05f62
Afonso Bordado <afonsobordado@az8.co>
2024-05-31 21:13:17 +0100

    Allocate JIT Context

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,56 @@
-use core::ffi::c_int;
-
 pub mod pg;
 
-#[no_mangle]
-pub extern "C" fn cranelift_add(left: c_int, right: c_int) -> c_int {
-    println!("In Rust: left = {}, right = {}", left, right);
-    left + right
+#[repr(C)]
+struct PgCraneliftContext {
+    base: pg::JitContext,
 }
 
 #[no_mangle]
-pub extern "C" fn _cranelift_compile_expr(_state: pg::ExprState) -> bool {
-    // pg::elog
+pub extern "C" fn _cranelift_compile_expr(state: *mut pg::ExprState) -> bool {
     println!("In Rust: cranelift_compile_expr");
-    cranelift_add(1, 2);
+
+    // Make this a reference so that it's easier to work with
+    assert!(!state.is_null());
+    let state = unsafe { &mut *state };
+
+    // Parent should never be null
+    assert!(!state.parent.is_null());
+    let parent = unsafe { &mut *state.parent };
+
+    assert!(!parent.state.is_null());
+    let estate = unsafe { &mut *parent.state };
+
+    let jit_ctx_ptr: *mut PgCraneliftContext = if !estate.es_jit.is_null() {
+        // This means that we have previously created a JIT context, lets reuse it
+        estate.es_jit.cast::<PgCraneliftContext>()
+    } else {
+        unsafe {
+            pg::ResourceOwnerEnlargeJIT(pg::CurrentResourceOwner);
+
+            let alloc = pg::MemoryContextAllocZero(
+                pg::TopMemoryContext,
+                std::mem::size_of::<PgCraneliftContext>(),
+            );
+
+            let jit_ctx = alloc.cast::<PgCraneliftContext>();
+            (*jit_ctx).base.flags = estate.es_jit_flags;
+
+            /* ensure cleanup */
+            (*jit_ctx).base.resowner = pg::CurrentResourceOwner;
+            pg::ResourceOwnerRememberJIT(pg::CurrentResourceOwner, jit_ctx as pg::Datum);
+
+            /* For re-using the JIT context. */
+            estate.es_jit = &mut (*jit_ctx).base;
+
+            jit_ctx
+        }
+    };
+
+    let _jit_ctx = unsafe { &mut (*jit_ctx_ptr) };
+
+    dbg!(jit_ctx_ptr);
+
+    /* Returning 'false' indicates we won't jit the current expression. */
     false
 }
 
@@ -25,23 +63,3 @@ pub extern "C" fn _cranelift_release_context(_ctx: *mut pg::JitContext) {
 pub extern "C" fn _cranelift_reset_after_error() {
     println!("In Rust: cranelift_reset_after_error");
 }
-
-// /* Function prototypes for JIT compilation. */
-// static bool cranelift_compile_expr(ExprState *state) {
-//     /*
-//      * Emit a notice message so that we can check if the JIT provider being
-//      * loaded successfully.
-//      */
-//     elog(NOTICE, "cranelift_compile_expr");
-
-//     cranelift_add(1, 2);
-
-//     /* Returning 'false' indicates we won't jit the current expression. */
-//     return false;
-//   }
-//   static void cranelift_release_context(JitContext *ctx) {
-//     elog(NOTICE, "cranelift_release_context");
-//   }
-//   static void cranelift_reset_after_error(void) {
-//     elog(NOTICE, "cranelift_reset_after_error");
-//   }
